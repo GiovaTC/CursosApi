a2500f568a940fadcf9dd2d10a78147ffa2491e3
gtapiero <giovanny.tapiero@pi.edu.co>
2025-06-03 15:22:53 -0500

    se adiciona builder.services.AddDbContext

--- CursosApi/Program.cs
+++ CursosApi/Program.cs
@@ -1,6 +1,12 @@
+using CursosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+        new MySqlServerVersion(new Version(8, 0, 30))));
 
 builder.Services.AddControllers();
 
