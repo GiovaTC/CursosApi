4da471cdd304d29bbc258cf7a3c5a4dd03fe756d
gtapiero <giovanny.tapiero@pi.edu.co>
2025-06-03 15:33:24 -0500

    commit - se adicionan Controllers

---
+++ CursosApi/Controllers/CursosController.cs
@@ -0,0 +1,26 @@
+﻿using CursosApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursosApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CursosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CursosController(AppDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetCursos() =>
+            Ok(await _context.Cursos.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> AddCurso(Curso curso)
+        {
+            _context.Cursos.Add(curso);
+            await _context.SaveChangesAsync();
+            return Ok(curso);
+        }
+    }
+}
---
+++ CursosApi/Controllers/InscripcionesController.cs
@@ -0,0 +1,26 @@
+﻿using CursosApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursosApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InscripcionesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public InscripcionesController(AppDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetCursos() =>
+            Ok(await _context.Cursos.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> AddCurso(Curso curso)
+        {
+            _context.Cursos.Add(curso);
+            await _context.SaveChangesAsync();
+            return Ok(curso);
+        }
+    }
+}
