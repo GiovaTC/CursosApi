1a8095103c3c8ec75d0ec9246d405870de388ba1
gtapiero <giovanny.tapiero@pi.edu.co>
2025-06-03 15:17:06 -0500

    commit - se elimina dbcontext del models y se implementa en Data

--- CursosApi/Data/AppDbContext.cs
+++ CursosApi/Data/AppDbContext.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using CursosApi.Models;
 
-namespace CursosApi.Data
+namespace CursosApi.Models
 {
     public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
         public DbSet<Curso> Cursos { get; set; }
+
         public DbSet<Estudiante> Estudiantes { get; set; }
         public DbSet<Inscripcion> Inscripciones { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inscripcion>()
+                .HasOne(i => i.Curso)
+                .WithMany(c => c.Inscripciones)
+                .HasForeignKey(i => i.CursoId);
 
-
-
+            modelBuilder.Entity<Inscripcion>()
+                .HasOne(i => i.Estudiante)
+                .WithMany(e => e.Inscripciones)
+                .HasForeignKey(i => i.EstudianteId);
+        }
     }
 }
